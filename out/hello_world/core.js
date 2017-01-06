fed04e34849c5a883684c180b5618ab1f444aef1
zanjs <root@zanjs.com>
2017-01-06 10:59:18 +0800

    :cat:

---
+++ out/hello_world/core.js
@@ -0,0 +1,7 @@
+// Compiled by ClojureScript 1.9.293 {}
+goog.provide('hello_world.core');
+goog.require('cljs.core');
+cljs.core.enable_console_print_BANG_.call(null);
+cljs.core.println.call(null,"Hello world! zan");
+
+//# sourceMappingURL=core.js.map
